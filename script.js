c5e8d4a4aee3736ccec647b831da6680a2e4b6b9
aleshaqqe <leshatk765@gmail.com>
2025-09-26 14:21:31 +0300

    Debug project

--- script.js
+++ script.js
@@ -4,27 +4,32 @@ document.addEventListener("DOMContentLoaded", () => {
   const btn=document.querySelector("button");
   btn.addEventListener("click", function(){
     let number = Math.floor((Math.random() * 100));
-    const value = parseFloat(input.value);
-
-    if(isNaN(value)) {
-    result.innerHTML=`<p>Введите число!</p>`;
-    input.value='';
-    return;
-  }
-
-  if(value==number){
-    result.innerHTML=`<p>Вы угадали число</p>`;
-    input.value='';
-
-  }else if(value!=number){
-    result.innerHTML=`<p>Попробуйте ещё раз</p>`
-    input.value='';
-  }else{
-    input.value='';
-    return 1;
-  }
+    const value = parseFloat(input.value.trim());
     const form = document.querySelector("form");
-    form.appendChild(result);
+    if (value === "" || isNaN(parseFloat(value))) {
+      result.innerHTML = `<p>Введите число!</p>`;
+      input.value = '';
+      form.appendChild(result);
+      return;ds
+    }
+
+    if(value==number){
+      result.innerHTML=`<p>Вы угадали число</p>`;
+      input.value='';
+      form.appendChild(result);
+
+
+    }else if(value!=number){
+      result.innerHTML=`<p>Попробуйте ещё раз</p>`
+      input.value='';
+      form.appendChild(result);
+
+    }else{
+      input.value='';
+      return 1;
+
+    }
+
     console.log(number);
     console.log(value);
   });
